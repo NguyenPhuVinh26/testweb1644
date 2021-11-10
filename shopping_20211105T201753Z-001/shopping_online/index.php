a5351b0d96aa7489a4fd072495d3654eab1c7339
NguyenPhuVinh26 <91648114+NguyenPhuVinh26@users.noreply.github.com>
2021-11-10 20:09:55 +0700

    .

--- shopping_20211105T201753Z-001/shopping_online/index.php
+++ shopping_20211105T201753Z-001/shopping_online/index.php
@@ -218,22 +218,22 @@
 
     <!-- Right -->
     <div>
-      <a href="" class="me-4 text-reset">
+      <a href="https://www.facebook.com/profile.php?id=100012763472006" class="me-4 text-reset">
         <i class="fab fa-facebook-f"></i>
       </a>
-      <a href="" class="me-4 text-reset">
+      <a href="https://www.facebook.com/profile.php?id=100012763472006" class="me-4 text-reset">
         <i class="fab fa-twitter"></i>
       </a>
-      <a href="" class="me-4 text-reset">
+      <a href="https://www.facebook.com/profile.php?id=100012763472006" class="me-4 text-reset">
         <i class="fab fa-google"></i>
       </a>
-      <a href="" class="me-4 text-reset">
+      <a href="https://www.facebook.com/profile.php?id=100012763472006" class="me-4 text-reset">
         <i class="fab fa-instagram"></i>
       </a>
-      <a href="" class="me-4 text-reset">
+      <a href="https://www.facebook.com/profile.php?id=100012763472006" class="me-4 text-reset">
         <i class="fab fa-linkedin"></i>
       </a>
-      <a href="" class="me-4 text-reset">
+      <a href="https://www.facebook.com/profile.php?id=100012763472006" class="me-4 text-reset">
         <i class="fab fa-github"></i>
       </a>
     </div>
@@ -318,7 +318,7 @@
   <!-- Copyright -->
   <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
     © 2021 Copyright:
-    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">NguyenPhuVinh.com</a>
   </div>
   <!-- Copyright -->
 </footer>
